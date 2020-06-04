262e3656dcfd643662517153797258a12ef07e06
Mohammad Mohsen-Pour <mohammad.mohsenpour1@gmail.com>
2020-06-04 11:48:22 -0400

    connected menu items to go to take the user to shop page showing all the collection items for that category

--- src/redux/directory/directoryReducer.js
+++ src/redux/directory/directoryReducer.js
@@ -5,35 +5,35 @@ const initialState = {
       imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
       size: 'small',
       id: 1,
-      linkUrl: 'hats'
+      linkUrl: 'shop/hats'
     },
     {
       title: 'JACKETS',
       imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
       size: 'small',
       id: 2,
-      linkUrl: ''
+      linkUrl: 'shop/jackets'
     },
     {
       title: 'SNEAKERS',
       imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
       size: 'small',
       id: 3,
-      linkUrl: ''
+      linkUrl: 'shop/sneakers'
     },
     {
       title: 'WOMENS',
       imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
       size: 'large',
       id: 4,
-      linkUrl: ''
+      linkUrl: 'shop/jackets'
     },
     {
       title: 'MENS',
       imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
       size: 'large',
       id: 5,
-      linkUrl: ''
+      linkUrl: 'shop/mens'
     }
   ]
 };
--- src/redux/shop/shopSelectors.js
+++ src/redux/shop/shopSelectors.js
@@ -1,7 +1,22 @@
 import { createSelector } from 'reselect';
 
+const COLLECTION_ID_MAP = {
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5,
+
+}
+
 const selectShop = (state) => state.shop;
 
+// Selector to retrive the top level collections for our shop page
 export const selectShopCollections = createSelector(
   [selectShop],
   (shop) => shop.collections)
+
+// Selector to retrive the items inside the collection to show on shop/{collection} page
+export const selectCollection = collectionUrlParam =>
+  createSelector([selectShopCollections],
+    collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]))
